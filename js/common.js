7623a4ea3ab6a2723e0cd20c8e4b759b3fb4c1af
Anatoliy <cata07@yandex.ru>
2016-07-20 13:12:38 +0300

    Сверстал макет главной
    Сетка резина.

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
 
+
+/*Подключение слайдера */
+jQuery(document).ready(function($) {
+	$('.slaider').unslider({
+		// autoplay:true,
+		arrows:false
+		});
+	$('.reviews').unslider({
+		// autoplay:true,
+		arrows:false
+		});
 });
+
+
+
+
+// воспроизводим видео по клику
+var handler = function () {
+	this.innerHTML = '<iframe src="https://www.youtube.com/embed/Q8v2WIvEJ3c" frameborder="0" border="0" allowfullscreen></iframe>;'};
+document.querySelector(".watch__video").addEventListener('click', function (e) {
+	this.removeEventListener('click', handler, false); handler.apply(this, arguments);
+ } , false);
